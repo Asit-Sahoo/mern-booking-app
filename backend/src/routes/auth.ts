e3c0b05498ae5bc65339d30e2d19f95189216454
Asit Sahoo <110890707+Asit-Sahoo@users.noreply.github.com>
2024-04-25 17:24:21 +0530

    Update auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -62,7 +62,7 @@ router.post(
   
     router.post("/logout", (req: Request, res: Response) => {
     res.cookie("auth_token", "", {
-      expires: new Date(0),
+     // expires: new Date(0),
     });
     res.send();
   });
